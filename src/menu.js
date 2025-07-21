ba4de863be61873d283ac2ed6574a0a1d64cd972
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-07-21 16:51:53 -0700

    feat: create separate tab for menu
    Implement tab browsing to access separate pages.

--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,17 @@
 const dishesContext = require.context("./images/dishes", false, /\.(png|svg|jpg|jpeg|gif)$/i);
 const dishes = dishesContext.keys().map(dishesContext);
 
+const button = document.getElementById("menu");
 const content = document.getElementById("content");
 
-dishes.forEach(dish => {
-    const image = document.createElement("img");
-    image.src = dish;
-    content.appendChild(image);
-});
+function displayMenu() {
+    content.textContent = "";
+
+    dishes.forEach(dish => {
+        const image = document.createElement("img");
+        image.src = dish;
+        content.appendChild(image);
+    });
+}
+
+button.addEventListener("click", displayMenu);
