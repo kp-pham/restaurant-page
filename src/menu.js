e75951976a727328608a105565f89bfd1099047d
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-07-21 17:23:20 -0700

    refactor: import image metadata
    Use image metadata to load images from directory.

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,6 @@
+import data from "./images/data/dishes.json";
+
 const dishesContext = require.context("./images/dishes", false, /\.(png|svg|jpg|jpeg|gif)$/i);
-const dishes = dishesContext.keys().map(dishesContext);
 
 const button = document.getElementById("menu");
 const content = document.getElementById("content");
@@ -7,9 +8,10 @@ const content = document.getElementById("content");
 function displayMenu() {
     content.textContent = "";
 
-    dishes.forEach(dish => {
+    data.forEach(dish => {
         const image = document.createElement("img");
-        image.src = dish;
+        image.src = dishesContext(`./${dish["file"]}`);
+        image.alt = dish["name"];
         content.appendChild(image);
     });
 }
