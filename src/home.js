c07fa72e23f73c52e89b7160f1315302f0515007
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-07-21 21:15:57 -0700

    refactor: create containers for images
    Create slideshow to implement carousel for images of restaurant interior.

--- src/home.js
+++ src/home.js
@@ -11,14 +11,24 @@ function displayHomepage() {
 function createSlideshow() {
     const slideshow = document.createElement("div");
     slideshow.classList.add("slideshow");
-
-    interiorImages.forEach(image => {
-        const slide = document.createElement("img");
-        slide.src = image;
-        slideshow.appendChild(slide);
-    });
+    
+    interiorImages.forEach(image => slideshow.appendChild(createSlide(image)));
 
     return slideshow;
+}
+
+function createSlide(image) {
+    const slide = document.createElement("div");
+    slide.appendChild(createSlideContent(image));
+
+    return slide;
+}
+
+function createSlideContent(image) {
+    const slideContent = document.createElement("img");
+    slideContent.src = image;
+
+    return slideContent;
 }
 
 export default displayHomepage;
