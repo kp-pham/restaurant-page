7b9aeaab756b9732163e4df3defe556ee32140ad
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-07-21 22:13:40 -0700

    feat: default export function to display contact page
    Implement tabbed browsing in entry point.

---
+++ src/contact.js
@@ -0,0 +1,7 @@
+const content = document.getElementById("content");
+
+function displayContact() {
+    content.textContent = "";
+}
+
+export default displayContact;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import displayHomepage from "./home.js";
 import displayMenu from "./menu.js";
+import displayContact from "./contact.js";
 import "./styles.css";
 
 const homeButton = document.getElementById("home");
 const menuButton = document.getElementById("menu");
+const contactButton = document.getElementById("contact");
 
 homeButton.addEventListener("click", displayHomepage);
 menuButton.addEventListener("click", displayMenu);
+contactButton.addEventListener("click", displayContact);
 
 displayHomepage();
