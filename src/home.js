3f5eb2bdfae0a5ca386521912bdefb9fcde631c1
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-07-21 21:07:15 -0700

    feat: default export function for displaying menu
    Implement tabbed browsing in entry point.

--- src/home.js
+++ src/home.js
@@ -1,31 +1,24 @@
-import skylightImage from "./images/interior/skylight.png";
+const interiorContext = require.context("./images/interior", false, /\.(png|svg|jpg|jpeg|gif)$/i);
+const interiorImages = interiorContext.keys().map(interiorContext);
 
 const content = document.getElementById("content");
 
-const image = document.createElement("img");
-image.src = skylightImage;
-image.alt = "Picture of skylight on the balcony of the second floor.";
-image.style.width = "500px";
-image.style.height = "auto";
+function displayHomepage() {
+    content.textContent = "";
+    content.appendChild(createSlideshow());
+}
 
-content.appendChild(image);
+function createSlideshow() {
+    const slideshow = document.createElement("div");
+    slideshow.classList.add("slideshow");
 
-const heading = document.createElement("h1");
-heading.textContent = "Hotel Debord";
+    interiorImages.forEach(image => {
+        const slide = document.createElement("img");
+        slide.src = image;
+        slideshow.appendChild(slide);
+    });
 
-content.appendChild(heading);
+    return slideshow;
+}
 
-const quote = document.createElement("p");
-quote.textContent = "Art is the key ingredient in fine dining.";
-
-content.appendChild(quote);
-
-const advertisement = document.createElement("p");
-advertisement.textContent = "Light and shadow weave a tapestry as dreams and reality merge and collide. Can you see through it, and find the truth hidden by the magic?";
-
-content.appendChild(advertisement);
-
-const callToAction = document.createElement("p");
-callToAction.textContent = "Tonight, the most dazzling twin stars of the Court of Fontaine shall reveal the secret!";
-
-content.appendChild(callToAction);
+export default displayHomepage;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import "./home.js";
+import displayHomepage from "./home.js";
 import displayMenu from "./menu.js";
 import "./styles.css";
 
+const homeButton = document.getElementById("home");
 const menuButton = document.getElementById("menu");
 
+homeButton.addEventListener("click", displayHomepage);
 menuButton.addEventListener("click", displayMenu);
+
+displayHomepage();
