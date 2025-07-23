5e6da292c6a55c4b4822861eaa76a03c7705081c
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-07-23 10:13:14 -0700

    refactor: create function for handling changing tabs
    Have visual indication in navigation for tabs opened.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,45 @@ import displayMenu from "./menu.js";
 import displayContact from "./contact.js";
 import "./styles.css";
 
-const homeButton = document.getElementById("home");
-const menuButton = document.getElementById("menu");
-const contactButton = document.getElementById("contact");
+const nav = document.querySelector("nav");
 
-homeButton.addEventListener("click", displayHomepage);
-menuButton.addEventListener("click", displayMenu);
-contactButton.addEventListener("click", displayContact);
+const HOMEPAGE = "home";
+const MENU = "menu";
+const CONTACT = "contact";
 
-displayHomepage();
+function clickHandlerNav(event) {
+    if (event.target.tagName === "BUTTON") changeTab(event);
+}
+
+function changeTab(event) {    
+    const tab = event.target.id;
+    
+    closeCurrentTab();
+    openTab(tab);
+
+    switch(tab) {
+        case HOMEPAGE:
+            displayHomepage();
+            break;
+        case MENU:
+            displayMenu();
+            break;
+        case CONTACT:
+            displayContact();
+            break;
+    }
+}
+
+function closeCurrentTab() {
+    const currentTab = document.querySelector(".active");
+    currentTab.classList.remove("active");
+}
+
+function openTab(tab) {
+    const newTab = document.getElementById(tab);
+    newTab.classList.add("active");
+}
+
+nav.addEventListener("click", clickHandlerNav);
+
+displayHomepage(); 
